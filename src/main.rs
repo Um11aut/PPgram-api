9b9445fd4f314bc7a0900d6d81a025ee2e275770
Um11aut <manokhin.pavlo@lll.kpi.ua>
2024-07-21 01:49:05 +0200

    delete unneccessary code

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,6 @@ use log::LevelFilter;
 use server::server::Server;
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
 
-async fn handle_connection(stream: &mut TcpStream, addr: SocketAddr) {
-    let mut buf = [0; 1024];
-
-    match stream.read(&mut buf).await {
-        Ok(n) if n == 0 => return,
-        Ok(n) => {
-            if let Err(e) = stream.write_all(&buf[0..n]).await {
-                eprintln!("failed to write to socket; err = {:?}", e);
-            }
-        }
-        Err(e) => {
-            eprintln!("failed to read from socket; err = {:?}", e);
-        }
-    }
-}
-
 #[tokio::main]
 async fn main() {
     env_logger::init();
